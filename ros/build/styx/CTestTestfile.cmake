ee184ba27ab25a6e2cb485a6021121d1706c6e2f
Thomas Luvö <thomas.luvo@zenuity.com>
2017-11-27 15:42:50 +0100

    Cleaned repo from build files

--- ros/build/styx/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/student/team-CarND/sdc-system-integration/ros/src/styx
-# Build directory: /home/student/team-CarND/sdc-system-integration/ros/build/styx
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- ros/build/tl_detector/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/student/team-CarND/sdc-system-integration/ros/src/tl_detector
-# Build directory: /home/student/team-CarND/sdc-system-integration/ros/build/tl_detector
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
