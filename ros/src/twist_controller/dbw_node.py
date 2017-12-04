67350ddc0654be75401ef623027a6fab61f94305
Satish Nandi <satish@nandi.net>
2017-12-04 01:36:06 -0800

    Hand tuned to work properly

--- ros/src/twist_controller/dbw_node.py
+++ ros/src/twist_controller/dbw_node.py
@@ -84,8 +84,8 @@ class DBWNode(object):
         
 
     def loop(self):
-        #rate = rospy.Rate(50) # 50Hz
-        rate = rospy.Rate(20) # 50Hz
+        rate = rospy.Rate(100) # 50Hz
+        #rate = rospy.Rate(20) # 50Hz
         while not rospy.is_shutdown():
             # TODO: Get predicted throttle, brake, and steering using `twist_controller`
             # You should only publish the control commands if dbw is enabled
